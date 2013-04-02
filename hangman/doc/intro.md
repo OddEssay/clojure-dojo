5c9a20da553f07c537dd0ebda14f070d81b032cc
Paul Freeman <paul@mojo-projects.com>
2013-04-02 19:16:39 +0100

    Removed basic project

--- hangman/doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to hangman
-
-TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
--- hangman/test/hangman/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns hangman.core-test
-  (:use clojure.test
-        hangman.core))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
